423cb0136593cbdcbffa4a3ccbb6a05c91fe3a5b
Mateusz Giełżecki <mateuszgielzecki@gmail.com>
2022-03-17 23:09:53 +0100

    add searching feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,17 +1,20 @@
+import * as model from './model.js';
+import recipeView from './views/recipeView.js';
+import searchView from './views/searchView.js';
+import resultsView from './views/resultsView.js';
+
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
-import * as model from './model.js'
-import recipeView from './views/recipeView.js';
-import recipeView from './views/recipeView.js';
+import { async } from 'regenerator-runtime';
 
-const recipeContainer = document.querySelector('.recipe');
+if (module.hot) {
+  module.hot.accept();
+}
 
-///////////////////////////////////////
 const controlRecipes = async function () {
   try {
 
     const id = window.location.hash.slice(1);
-    console.log(id);
 
     if (!id) return
     recipeView.renderSpinner();
@@ -23,10 +26,28 @@ const controlRecipes = async function () {
     recipeView.render(model.state.recipe);
   } catch (err) {
     recipeView.renderError()
+  };
+};
+
+const controlSearchResults = async function () {
+  try {
+    resultsView.renderSpinner();
+    //1. Get seach query
+    const query = searchView.getQuery();
+    if (!query) return;
+
+    // 2. Load search results
+    await model.loadSearchResults(query);
+
+    // 3. Render results
+    resultsView.render(model.state.search.results);
+  } catch (err) {
+    console.log(err);
   }
-}
+};
 
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
+  searchView.addHandlerSearch(controlSearchResults)
 }
 init();
